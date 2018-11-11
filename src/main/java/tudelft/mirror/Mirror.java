b9ebaeeb8b194b81ad7ec557ad9d18b48126ac21
azadorozhnuk <azadorozhnuk@intersog.com>
2018-11-11 15:30:32 +0400

    Mirror method is fixed and tests are written

--- src/main/java/tudelft/mirror/Mirror.java
+++ src/main/java/tudelft/mirror/Mirror.java
@@ -2,20 +2,18 @@ package tudelft.mirror;
 
 public class Mirror {
 
-    public String mirrorEnds(String string) {
+    public String mirrorEnds(String string){
         String mirror = "";
-
         int begin = 0;
-        int end = string.length() - 1;
-        for (; begin < end; begin++, end--) {
-            if (string.charAt(begin) == string.charAt(end)) {
+        int end =  string.length() -1;
+        for (; begin  < end; begin++, end--){
+            if(string.charAt(begin) == string.charAt(end)){
                 mirror += String.valueOf(string.charAt(end));
             }
             else {
                 break;
             }
         }
-
-        return begin == end ? string : mirror;
+        return begin >= end ? string : mirror;
     }
 }
--- src/test/java/tudelft/mirror/MirrorTest.java
+++ src/test/java/tudelft/mirror/MirrorTest.java
@@ -1,4 +1,21 @@
 package tudelft.mirror;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import tudelft.chocolate.ChocolateBags;
+
 public class MirrorTest {
+
+
+    @ParameterizedTest(name = "{0} = {1}")
+    @CsvSource({
+            // The total is higher than the amount of small and big bars.
+            "'abXhjXba', 'abX'", "'aba', 'aba'", "'aa', 'aa'", "'a', 'a'", "'', ''"
+    })
+    public void testAlgorithm(String str, String result) {
+        Mirror m = new Mirror();
+        String actual = m.mirrorEnds(str);
+        Assertions.assertEquals(result, actual);
+    }
 }
