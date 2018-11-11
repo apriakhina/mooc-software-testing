5cfc3d2ef8a789483fc421d8ecb7b29e3b643087
azadorozhnuk <azadorozhnuk@intersog.com>
2018-11-11 16:05:58 +0400

    Fixed method CaesarShiftCipher and written tests for it

--- src/main/java/tudelft/caesarshift/CaesarShiftCipher.java
+++ src/main/java/tudelft/caesarshift/CaesarShiftCipher.java
@@ -3,6 +3,8 @@ package tudelft.caesarshift;
 public class CaesarShiftCipher {
 
     public String CaesarShiftCipher(String message, int shift){
+        if(message.isEmpty())
+            return "invalid";
         StringBuilder sb = new StringBuilder();
         char currentChar;
         int length = message.length();
@@ -11,11 +13,10 @@ public class CaesarShiftCipher {
 
         for(int i = 0; i < length; i++){
             currentChar = message.charAt(i);
-           
-            sb.append(currentChar);
-            if (currentChar > 'z' || currentChar < 'a') {
+
+            if (currentChar > 'z' || currentChar < 'a')
                 return "invalid";
-            } else if ((char) (currentChar + shift) > 'z') {
+            else if ((char) (currentChar + shift) > 'z') {
                 currentChar = (char) (currentChar - 26);
             } else if ((char) (currentChar + shift) < 'a'){
                 currentChar = (char) (currentChar + 26);
--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,23 @@
 package tudelft.caesarshift;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+
 public class CaesarShiftCipherTest {
+
+
+    @ParameterizedTest(name = "({0}, {1}) = {2}")
+    @CsvSource({
+            // The total is higher than the amount of small and big bars.
+            "'abc', 0, 'abc'", "'abc', 26, 'abc'", "'abc', -1, 'zab'", "'abc', -3, 'xyz'", "'abc', 3, 'def'", "'xyz', 5, 'cde'",
+            "'22', '1', 'invalid'", "'Qwse', '2', 'invalid'", "'', 2, 'invalid'"
+    })
+    public void testAlgorithm(String message, int shift, String expected) {
+        CaesarShiftCipher c = new CaesarShiftCipher();
+        String actual = c.CaesarShiftCipher(message, shift);
+        Assertions.assertEquals(expected, actual);
+    }
 
 }
