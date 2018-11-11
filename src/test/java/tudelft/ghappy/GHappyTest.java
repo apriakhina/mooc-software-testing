e5c46495e171848bbf2b9f18178a22a81c8239d4
azadorozhnuk <azadorozhnuk@intersog.com>
2018-11-11 15:41:10 +0400

    Fixed gHappy method and written tests for it

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,20 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+
 public class GHappyTest {
+
+
+    @ParameterizedTest(name = "{0} = {1}")
+    @CsvSource({
+            // The total is higher than the amount of small and big bars.
+            "'ggh', true", "'hgh', false", "'hgg', true", "'g', false", "'gg', true", "'xxggyygxx', false",
+    })
+    public void testAlgorithm(String str, boolean result) {
+        GHappy g = new GHappy();
+        Assertions.assertEquals(result, g.gHappy(str));
+    }
 }
